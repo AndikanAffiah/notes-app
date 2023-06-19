bd8bb655ffb022704cbf746dc3aa26ce0a4ac045
AndikanAffiah <andikanaffiah@gmail.com>
2023-06-19 20:38:00 +0100

    added components

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import '@/assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+// @ts-ignore
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate"
+const pinia = createPinia();
+
+import App from './App.vue'
+
+const app = createApp(App)
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
+
+app.mount('#app')
